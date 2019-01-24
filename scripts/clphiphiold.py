1c4c93e0e9da086c85b3779ffee232e9cb7cb6d9
modichirag <modichirag@berkeley.edu>
2019-01-23 18:05:25 -0800

    clphiphi with getcl

--- scripts/clphiphiold.py
+++ scripts/clphiphiold.py
@@ -21,6 +21,13 @@ print(wsize, rank)
 def lensing_kernel(xi, xmax):
     return (xmax - xi)/(xmax*xi) * (xmax > xi) * (1.+z_chi(xi))
 
+##r2d, t2d = np.meshgrid(t_,t_)
+##w11, w12 = np.meshgrid(w1,w1)
+### inflate by one dimensions (nu_n)
+##r2d, t2d = np.expand_dims(r2d, 2), np.expand_dims(t2d, 2)
+##w11, w12 = np.expand_dims(w11, 2), np.expand_dims(w12, 2)
+###I0_ltrc  = np.swapaxes(I0_lcrt, 1, 3)
+##
 
 ####################
 outpath = './output/clphiphi2/'
@@ -38,19 +45,36 @@ if rank == 0: np.savetxt(outpath + 'chis.txt', chi1maxs)
 if rank == 0: print('chi1 split amongst ranks = ', chi1maxsplit)
 
 #indexsplit = [[99]]
-kernel1 = lensing_kernel
-kernel2 = lensing_kernel
-
 for index in indexsplit[rank]:
-    print('Rank %d for index '%rank, index)
+    print(index)
     chi1max = chi1maxs[index]
     
     begin=time()
+
     tosave = np.zeros((ell_.size, chi2maxs.size+1))
     tosave[:, 0] = ell_
     for ichi2, chi2max in enumerate(chi2maxs[::]):
         
+        kernel1 = lensing_kernel
+        kernel2 = lensing_kernel
         Cl = getcl(kernel1, kernel2, chi1max, chi2max, nushift=0, prefindex=2)
+
+##        chi1fac0 = (lensing_kernel(r2d*chi1max, chi1max)*D_chi(r2d*chi1max))
+##        chi1fac0 = chi1fac0 *(r2d*chi1max)**(1-nu_n_.reshape(1, 1, -1))
+##
+##        chi2fac00 = (lensing_kernel(t2d*r2d*chi1max, chi2max)*D_chi(r2d*t2d*chi1max))
+##        chi2fac01 = (lensing_kernel(1./t2d*r2d*chi1max, chi2max)*D_chi(r2d*1./t2d*chi1max))
+##        chi2fac01 = chi2fac01 * t2d**(nu_n_.reshape(1, 1, -1)-2)
+##        chi2fac0  = chi2fac00 + chi2fac01
+##
+##        chifacs = w11*w12*chi1fac0* chi2fac0
+##
+##        result=np.zeros_like(ell_)
+##        for ii  in range(ell_.size):        
+##            result[ii] = np.sum(chifacs*I0_ltrc[ii])
+##
+##        Cl = chi1max * result*1./np.pi**2/2. * prefac**2
+##
         tosave[:, ichi2+1] = Cl
 
     np.savetxt(outpath + '%d.txt'%index, tosave, fmt='%0.4e', header='ell, chi2')
