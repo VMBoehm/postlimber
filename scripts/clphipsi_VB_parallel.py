97706d8ac4fff3feb2160d45a596a8ef6e0f946c
Vanessa Boehm <vboehm@mpa-garching.mpg.de>
2019-01-25 14:33:07 -0800

    first results of parallelized clpsiphi code of vanessa

--- scripts/clphipsi_VB_parallel.py
+++ scripts/clphipsi_VB_parallel.py
@@ -23,14 +23,14 @@ w1d = w1.reshape(-1, 1)
 Cl = np.zeros((len(ell_),len(t_),len(t_)))
 I0_ltc = np.squeeze(I0_ltrc,axis=2)
 
-junksize = int(len(t_)/size)
-max_num  = max((rank+1)*junksize,size)
-jjs      = np.arange(rank*junksize, max_num)
+junksize = np.ceil(len(t_)/size)
+max_num  = min((rank+1)*junksize,len(t_))
+jjs      = np.arange(rank*junksize, max_num,dtype=np.int)
 print(junksize,max_num)
 
-Cl = np.zeros((len(ell_),len(jjs),len(t_)))
+Cl = np.zeros((len(jjs),len(ell_),len(t_)))
 
-for chi1_max in ((t_*chi_cmb)[jjs]):
+for jj, chi1_max in enumerate((t_*chi_cmb)[jjs]):
     for ii, chi2_max in enumerate((t_*chi_cmb)):
 
 
@@ -50,12 +50,16 @@ for chi1_max in ((t_*chi_cmb)[jjs]):
 
       result = np.zeros_like(ell_)
 
-      Cl[:,jj,ii] = np.sum(chifacs*I0_ltc, axis=(1,2)) #not summing over r
+      Cl[jj,:,ii] = np.real(np.sum(chifacs*I0_ltc, axis=(1,2))) #not summing over r
 
 result = comm.gather(Cl, root=0)
+ranks  = comm.gather(rank, root =0)
 
-print(result.shape)
 
-result*=(1./np.pi**2/2.*prefac**2)
 if rank ==0:
-  np.save('../G_matrices/clphipsi_parallel',Cl)
+    cl = np.vstack([result[ii] for ii in range(size)])
+    print(cl.shape)
+    np.swapaxes(cl,0,1)
+    print(cl.shape)
+    cl*=(1./np.pi**2/2.*prefac**2)
+    np.save('../G_matrices/clphipsi_parallel',cl)
