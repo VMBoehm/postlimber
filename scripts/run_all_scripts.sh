d7f9c7d767b631143ca2f42466b63681d2ae4d44
Vanessa Boehm <vboehm@mpa-garching.mpg.de>
2019-04-09 19:50:42 -0700

    plots for desi lunch...

--- scripts/run_all_scripts.sh
+++ scripts/run_all_scripts.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 module load python/3.6-anaconda-4.4
-paramfile=../settings/params_cross_gaussgal_z20_sigma5_cmblenssimple_bias_zmax10886.pkl
+paramfile=../settings/params_cross_gaussgal_z20_sigma1_deltalens_z17simple_bias_zmax25.pkl
 srun -n 25 python -u clphiphi_VB_parallel_source.py $paramfile
 srun -n 100 python -u clphigal_VB_parallel.py $paramfile
 srun -n 100 python -u clpsiphi_VB_parallel.py $paramfile
-paramfile=../settings/params_cross_gaussgal_z10_sigma4_cmblenssimple_bias_zmax10886.pkl
+paramfile=../settings/params_cross_gaussgal_z17_sigma2_deltalens_z17simple_bias_zmax25.pkl
 srun -n 25 python -u clphiphi_VB_parallel_source.py $paramfile
 srun -n 100 python -u clphigal_VB_parallel.py $paramfile
 srun -n 100 python -u clpsiphi_VB_parallel.py $paramfile
-paramfile=../settings/params_cross_gaussgal_z10_sigma1_cmblenssimple_bias_zmax10886.pkl
+paramfile=../settings/params_cross_gaussgal_z17_sigma2_deltalens_z23simple_bias_zmax25.pkl
 srun -n 25 python -u clphiphi_VB_parallel_source.py $paramfile
 srun -n 100 python -u clphigal_VB_parallel.py $paramfile
 srun -n 100 python -u clpsiphi_VB_parallel.py $paramfile
-paramfile=../settings/params_cross_gaussgal_z10_sigma1_deltalens_z7simple_bias_zmax15.pkl
+paramfile=../settings/params_cross_gaussgal_z7_sigma2_deltalens_z13simple_bias_zmax15.pkl
 srun -n 25 python -u clphiphi_VB_parallel_source.py $paramfile
 srun -n 100 python -u clphigal_VB_parallel.py $paramfile
 srun -n 100 python -u clpsiphi_VB_parallel.py $paramfile
