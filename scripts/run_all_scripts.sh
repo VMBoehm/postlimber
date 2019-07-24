d28ace1c8e00a76f8c6ec82eaea6a3a3ffc5e18a
Vanessa Boehm <boehmvanessa@gmail.com>
2019-07-24 01:27:29 -0700

    cross bias fully coded and tested

--- scripts/run_all_scripts.sh
+++ scripts/run_all_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 module load python/3.6-anaconda-4.4
 paramfile=../settings/params_lsstall_cmblens_simple_bias_zmax10886.pkl
-srun -n 25 python -u clphiphi_VB_parallel_source.py $paramfile
+srun -n 25 python -u clpsiphi_VB_parallel_magbias.py $paramfile
 #srun -n 100 python -u clphigal_VB_parallel.py $paramfile
-#srun -n 100 python -u clpsiphi_VB_parallel.py $paramfile
+#srun -n 25 python -u clpsiphi_VB_parallel.py $paramfile
